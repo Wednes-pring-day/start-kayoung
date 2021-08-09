a981abd992c74de8eded1a243d7ced655873c2f5
kayoung <rkdkdudjd@gmail.com>
2021-08-10 01:47:23 +0900

    feat: implementation of member repository using program memory

---
+++ src/main/kotlin/com/spring/wednes/startky/repository/MemoryMemberRepository.kt
@@ -0,0 +1,37 @@
+package com.spring.wednes.startky.repository
+
+import com.spring.wednes.startky.domain.Member
+import org.springframework.stereotype.Repository
+
+@Repository
+class MemoryMemberRepository: MemberRepository {
+
+    var store: HashMap<Long, Member> = HashMap()
+    var sequence: Long = 0L
+
+    override fun save(member: Member): Member {
+        sequence += 1
+        member.id = sequence
+        store.put(member.id!!, member)
+        return member
+    }
+
+    override fun findById(id: Long): Member? {
+        return store.get(id)
+    }
+
+    override fun findByName(name: String): Member? {
+        val res = store.values
+                .filter { member: Member -> member.name.equals(name) }
+        if (res.isEmpty()) {
+            return null
+        }
+        return res[0]
+    }
+
+    override fun findAll(): List<Member> = ArrayList(store.values)
+
+    fun clearStore() {
+        store.clear()
+    }
+}
