dd58dbbc60d9584efcc2fba3f723f45b0331dfba
kayoung <rkdkdudjd@gmail.com>
2021-08-10 01:46:14 +0900

    init: kotlin spring project

---
+++ src/test/kotlin/com/spring/wednes/startky/StartKyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spring.wednes.startky
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartKyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
