3f93ca51d69504ec4ac71c23a3b00a7193973162
kayoung <rkdkdudjd@gmail.com>
2021-08-10 14:55:59 +0900

    fix: member id to not nullable

--- src/main/kotlin/com/spring/wednes/startky/domain/Member.kt
+++ src/main/kotlin/com/spring/wednes/startky/domain/Member.kt
@@ -1,12 +1,10 @@
 package com.spring.wednes.startky.domain
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Table(name="member")
 data class Member(
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long?,
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long,
         var name: String
         )
